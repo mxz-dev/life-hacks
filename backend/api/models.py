f50df2ba8a5cf2fc194f035817cf4ff242541dde
mxz-dev <zabetzadehmohmmad@gmail.com>
2025-05-02 18:06:52 +0330

    staging ...

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True, null=True)
+    avatar = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False , related_name='following', blank=True)
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+class LifeHack(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lifehacks')
+    tag = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    pass
