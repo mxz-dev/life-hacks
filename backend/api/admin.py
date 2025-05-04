93a8ab353886d971eb41111d51df50066a1b2714
mxz-dev <zabetzadehmohmmad@gmail.com>
2025-05-04 12:13:36 +0330

    added models, and some tests in api/test

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Tag, UserProfile, LifeHack, Comment
+@admin.register(LifeHack)
+class LifeHackAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
--- backend/api/models.py
+++ backend/api/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True, null=True)
     avatar = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False , related_name='following', blank=True)
-    following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
+    followers = models.ManyToManyField('self', symmetrical=False ,related_query_name="following_user", related_name='following_set', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -15,9 +21,9 @@ class UserProfile(models.Model):
         return f'{self.user.username} Profile'
 class LifeHack(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lifehacks')
-    tag = models.CharField(max_length=100)
+    tag = models.ManyToManyField(Tag, related_name='tagged_lifehacks', blank=True, null=True)
     title = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -25,4 +31,10 @@ class LifeHack(models.Model):
         return self.title
     
 class Comment(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    hack = models.ForeignKey(LifeHack, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} on {self.hack.title}"
